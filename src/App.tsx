74885be3cccc632f283943b13eb06d25f857ded6
fatemehamedani <ftmehmdni1380@gmail.com>
2025-01-11 19:15:17 +0330

    event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { MouseEvent } from "react";
 import ListGroup from "./components/ListGroup";
 const App = () => {
+  let items = ["isfahan", "tehran", "shiraz", "tabriz", "rasht"];
+  const handleClick = (event : MouseEvent) => console.log(event);
   return (
     <>
       <div className="border rounded-lg shadow-lg p-4">
-        <h2>welcome</h2>
         <ul className="divide-y divide-gray-200">
           <h1 className="py-4 font-bold text-4xl">LIST</h1>
-          <li className="py-4">An item</li>
-          <li className="py-4">A second item</li>
-          <li className="py-4">A third item</li>
-          <li className="py-4">A fourth item</li>
-          <li className="py-4">And a fifth one</li>
+          {items.length === 0 && <p>No item found</p>}
+          {items.map((item , index) => (
+            <li className="p-4" key={item} onClick={handleClick}>
+              {item}
+            </li>
+          ))}
         </ul>
       </div>
     </>
