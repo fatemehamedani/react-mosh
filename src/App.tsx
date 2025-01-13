99ddaa735805933df11682322625a320f2dbd07e
fatemehamedani <ftmehmdni1380@gmail.com>
2025-01-14 00:25:24 +0330

    props/state

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { MouseEvent } from "react";
-import ListGroup from "./components/ListGroup";
-const App = () => {
-  let items = ["isfahan", "tehran", "shiraz", "tabriz", "rasht"];
-  const handleClick = (event : MouseEvent) => console.log(event);
-  return (
-    <>
-      <div className="border rounded-lg shadow-lg p-4">
-        <ul className="divide-y divide-gray-200">
-          <h1 className="py-4 font-bold text-4xl">LIST</h1>
-          {items.length === 0 && <p>No item found</p>}
-          {items.map((item , index) => (
-            <li className="p-4" key={item} onClick={handleClick}>
-              {item}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
-};
 
+import ListGroup from "./components/ListGroup";
+
+function App() { 
+  let items = ["isfahan", "tehran", "shiraz", "tabriz", "rasht"];
+
+  const handleSelectItem = (item: string) => {
+console.log(item);
+
+  }
+
+   return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    </div>
+  );
+}
 export default App;
+   
+
+
+
+
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,39 @@
-function ListGroup(){}
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+    heading: string;
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading , onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  return (
+    <>
+      <div className="border rounded-lg shadow-lg p-4">
+        <ul className=" divide-y divide-gray-200 ">
+                  <h1 className="py-4 font-bold text-4xl">{heading}</h1>
+          {items.length === 0 && <p>No item found</p>}
+          {items.map((item, index) => (
+            <li
+              className={
+                selectedIndex === index
+                  ? "p-4 list-group active:bg-blue-700"
+                  : "list-group"
+              }
+              key={item}
+              onClick={() => {
+                  setSelectedIndex(index);
+                  onSelectItem(item);
+              }}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
 
 export default ListGroup;
