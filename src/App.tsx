78c0803bd1d3e98f430831c7f2946d916ddd1775
fatemehamedani <ftmehmdni1380@gmail.com>
2025-01-17 19:17:31 +0330

    managing component state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import Like from "./components/Like/Like";
+import { useState } from "react"
+
 
 const App = () => {
+
+  const [tags, setTags] = useState(['happy', 'sad']);
+
+  const handleClick = () => {
+    // add
+    setTags([...tags, 'exciting']);
+    // remove
+    setTags(tags.filter(tag => tag !== 'happy'));
+    // update
+    setTags(tags.map(tag => tag === 'sad' ? tag : 'cheerful'));
+   };
+
   return (
     <div>
-      <Like onClick={() => console.log("clicked")
-      }/>
+      <button onClick={handleClick}>My Button</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
