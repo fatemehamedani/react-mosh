48d908b743f9726b44a6d87c9e69834aa718cacb
fatemehamedani <ftmehmdni1380@gmail.com>
2025-01-17 22:40:29 +0330

    Updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "john",
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    topping: ["Mushroom"],
   });
 
   const handleClick = () => {
-    setGame({ ...game, player: {...game.player, name: 'Bob'}})
+    setPizza({ ...pizza, topping: [...pizza.topping, "Cheese"] });
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
 }
 
 export default App;
