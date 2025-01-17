c3f1bbf3ce39cbc0a8b3baa2d9cad146413bc25e
fatemehamedani <ftmehmdni1380@gmail.com>
2025-01-17 19:47:02 +0330

    Update Logic with Immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
+import produce from "immer";
 
-
-const App = () => {
-
-  const [tags, setTags] = useState(['happy', 'sad']);
+function App() {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    // add
-    setTags([...tags, 'exciting']);
-    // remove
-    setTags(tags.filter(tag => tag !== 'happy'));
-    // update
-    setTags(tags.map(tag => tag === 'sad' ? tag : 'cheerful'));
-   };
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  };
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}{" "}
+        </p>
+      ))}
       <button onClick={handleClick}>My Button</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
