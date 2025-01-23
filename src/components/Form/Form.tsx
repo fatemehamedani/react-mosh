b53683aaab57882d1e02d97ecb3956546d3a31af
fatemehamedani <ftmehmdni1380@gmail.com>
2025-01-23 19:59:30 +0330

    Schema based Validation with zod

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,19 +1,22 @@
 import { useState, FormEvent } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import "./Form.css";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z.number({ invalid_type_error: "Age filed is required" }).min(5),
+});
 
-interface FormData {
-  name: string;
-  age: number;
-}
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -24,28 +27,24 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           type="text"
           className="border border-gray-600 rounded-md px-1 py-2"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-red-700">The name filed is required</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-red-700">The name must be at least 3 characters</p>
-        )}
+        {errors.name && <p className="text-red-700">{errors.name.message}</p>}
       </div>
       <div className="flex flex-col my-4 w-72 px-3">
         <label htmlFor="age" className="label mb-2">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="border border-gray-600 rounded-md px-1 py-2"
         />
+        {errors.age && <p className="text-red-700">{errors.age.message}</p>}
         <button className="bg-blue-600 text-white py-2 mt-4 w-28 rounded font-bold">
           Submit
         </button>
