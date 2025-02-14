93c1bb31af5a5596288ef03c5152214797c70772
fatemehamedani <ftmehmdni1380@gmail.com>
2025-02-14 23:47:59 +0330

    Connecting to the Backend

---
+++ src/App2.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import ProductList from "./components/ProductList";
+
+const App2 = () => {
+  useEffect(() => {
+    document.title = "My App";
+  });
+
+  const [category, setCategory] = useState("");
+
+  return (
+    <div className="ml-7">
+      <select
+        id="Form"
+        className="flex flex-col w-52 my-4 px-3 rounded border border-gray-700"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="clothing">Clothing</option>
+        <option value="household">Household</option>
+      </select>
+
+      <ProductList />
+    </div>
+  );
+};
+
+export default App2;
