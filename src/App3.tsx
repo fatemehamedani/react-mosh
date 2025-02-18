d08d4c6fb76cc19f1b9767c7cd6fc4cd5b5b4f51
fatemehamedani <ftmehmdni1380@gmail.com>
2025-02-18 13:48:44 +0330

    Deleting Data

--- src/App3.tsx
+++ src/App3.tsx
@@ -10,6 +10,16 @@ function App3() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users" + user.id)
+      .catch((error) => {
+        setError(error.message);
+        setUsers(originalUsers);
+      });
+  };
 
   useEffect(() => {
     const controller = new AbortController();
@@ -19,13 +29,13 @@ function App3() {
         signal: controller.signal,
       })
       .then((response) => {
-        setUsers(response.data) 
-        setLoading(false)
+        setUsers(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
-        setLoading(false)
+        setLoading(false);
       });
     return () => controller.abort();
   }, []);
@@ -33,11 +43,21 @@ function App3() {
   return (
     <>
       {error && <p className="text-red-600">{error}</p>}
-      {loading && <div className="w-8 h-8 border-4 border-gray-500 border-t-blue-600 rounded-full animate-spin"></div>}
-        
-     <ul className="ml-4">
+      {loading && (
+        <div className="w-8 h-8 border-4 border-gray-500 border-t-blue-600 rounded-full animate-spin"></div>
+      )}
+
+      <ul className=" ml-4 divide-y divide-gray-600">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="flex justify-between">
+            {user.name}
+            <button
+              className="text-gray-600 rounded-md border border-red-500 py-2 mt-4 w-24"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
